b9b13390dffca734a0a8f2e6a7fa8bed49177d8d
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-09-15 18:45:31 +0600

    ilim

--- Redux/reducer/store.tsx
+++ Redux/reducer/store.tsx
@@ -1,17 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ActivationReducerSlice from "./slice/ActivationReducerSlice";
 import UserInfoSlice from "./slice/UserInfoSlice";
+import selectedAddressReducer from "../address/addressesSlice";
 
-// Создание Redux store
 const store = configureStore({
   reducer: {
     auth: ActivationReducerSlice,
     users: UserInfoSlice,
+    selectedAddress: selectedAddressReducer,
   },
 });
 
-// Экспорт типов RootState и AppDispatch
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
 export default store;
