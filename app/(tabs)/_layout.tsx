6f089f1551e44cc3fa093df0f827cbe66ff4888c
Sanix6 <kylycbekovsanzar4@gmail.com>
2024-11-16 07:20:02 +0600

    ll5

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
         headerShown: false, 
         tabBarStyle: {
           ...(Platform.OS === 'android' && {
-            height:70
+            height:65
           }),
         },
       }}
